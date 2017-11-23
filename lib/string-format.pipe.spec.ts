4d06b15a0430b0b0f7faf1d764ffc370ba7f9d35
de Mijolla <mimexa@gmail.com>
2017-11-23 12:04:11 +0100

    tests mocha chai

---
+++ lib/string-format.pipe.spec.ts
@@ -0,0 +1,30 @@
+import 'zone.js';
+import 'reflect-metadata';
+import { StringFormatPipe } from './string-format.pipe';
+import { expect } from 'chai';
+
+describe('StringFormatPipe strings', () => {
+    
+  it('width', () => {
+    const pipe = new StringFormatPipe();
+    expect(pipe.transform('%10s', 'chuck')).to.equal('     chuck');
+    expect(pipe.transform('%2s', 'chuck')).to.equal('chuck');
+  });
+
+  it('index', () => {
+    const pipe = new StringFormatPipe();
+    expect(pipe.transform('%2$s %1$s', 'norris', 'chuck')).to.equal('chuck norris');
+  });
+
+
+});
+
+describe('StringFormatPipe numbers', () => {  
+  
+  it('standard', () => {
+    const pipe = new StringFormatPipe();
+    expect(pipe.transform('%d', 24)).to.equal('24.00');
+    expect(pipe.transform('%d', '24')).to.equal('24.00');
+  });
+  
+});
